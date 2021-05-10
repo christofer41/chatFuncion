7acaf3dfaaf0168ffdd63f523fbb3d84ea56a41d
jv98 <jacob@vahlen.se>
2021-05-10 15:41:37 +0200

    hiding emlets on page for chat and creatchat windows

--- public/script.js
+++ public/script.js
@@ -1,39 +1,51 @@
-const socket = io()
-let selectedRoom = "";
+const socket = io();
+let selectedRoom = '';
 let allRooms;
 
-
-
 //Our socket connections
-socket.on('Connected', renderChat)
-socket.on('update chat', renderMessage)
+socket.on('Connected', renderChat);
+socket.on('update chat', renderMessage);
 
 //We collect all the rooms that have been created
-fetch("http://localhost:3000/allRooms").then((response) => {
-    return response.json()
-}).then((rooms) => {
-    showAllRooms(rooms)
-})
-
-
+fetch('http://localhost:3000/allRooms')
+    .then((response) => {
+        return response.json();
+    })
+    .then((rooms) => {
+        showAllRooms(rooms);
+    });
 
 //Event listeners
-const messageButton = document.getElementById("message-button")
-messageButton.addEventListener('click', sendMessage)
+window.addEventListener('load', () => {
+    const messageButton = document.getElementById('message-button');
+    messageButton.addEventListener('click', sendMessage);
 
+    const homeButton = document.getElementById('home');
+    homeButton.addEventListener('click', renderHome);
+
+    const chatbutton = document.getElementById('public');
+    chatbutton.addEventListener('click', renderChatpage);
+
+    const creatroom = document.getElementById('roomcreat');
+    creatroom.addEventListener('click', renderCreatRooms);
+
+    const homeButton2 = document.getElementById('homes');
+    homeButton2.addEventListener('click', renderHome);
+
+     renderHome();
+});
 
 //What happens when we render the chat.
 function renderChat(socket) {
-     console.log("Hello");
-
+    console.log('Hello');
 }
 //What happens when we render the message
 function renderMessage(socket) {
-    const board = document.getElementById("message-here");
-    const theMessage = document.createElement("li");
-    console.log(socket)
-    theMessage.innerHTML = socket.username + ": " + socket.message;
-    board.append(theMessage)
+    const board = document.getElementById('message-here');
+    const theMessage = document.createElement('li');
+    console.log(socket);
+    theMessage.innerHTML = socket.username + ': ' + socket.message;
+    board.append(theMessage);
 }
 
 //The class for the room
@@ -46,95 +58,84 @@ class Room {
     }
 }
 
-
 //We render all rooms
 function showAllRooms(rooms) {
-    let lockedRoomBox = document.getElementById("private")
-    let unlockedRoomBox = document.getElementById("public")
+    let lockedRoomBox = document.getElementById('private');
+    let unlockedRoomBox = document.getElementById('public');
 
     //If no rooms exist we return null
-    if (!lockedRoomBox && !unlockedRoomBox){
+    if (!lockedRoomBox && !unlockedRoomBox) {
         return;
     }
 
-
-
-    rooms.forEach(rooms => {
-        console.log(rooms)
+    rooms.forEach((rooms) => {
+        console.log(rooms);
 
         if (rooms.locked) {
-            let roomTh = document.createElement("th");
-            let room = document.createElement("th");
+            let roomTh = document.createElement('th');
+            let room = document.createElement('th');
             room.innerText = rooms.id;
-            room.className = "lockedRooms"
-
+            room.className = 'lockedRooms';
 
             roomTh.appendChild(room);
             lockedRoomBox.appendChild(roomTh);
         } else {
-            let roomTh = document.createElement("th");
-            let room = document.createElement("th");
+            let roomTh = document.createElement('th');
+            let room = document.createElement('th');
             room.innerText = rooms.id;
-            room.className = "unlockedRooms"
-
+            room.className = 'unlockedRooms';
 
             roomTh.appendChild(room);
             unlockedRoomBox.appendChild(roomTh);
         }
 
         //Add event listeners
-    })
-    addEvents()
+    });
+    addEvents();
 }
 
 //We give all rooms a click event
 function addEvents() {
-    let lockedRoomBox = document.getElementsByClassName("lockedRooms")
-    let unlockedRoomBox = document.getElementsByClassName("unlockedRooms")
-
+    let lockedRoomBox = document.getElementsByClassName('lockedRooms');
+    let unlockedRoomBox = document.getElementsByClassName('unlockedRooms');
 
     for (let i = 0; i < lockedRoomBox.length; i++) {
-        lockedRoomBox[i].addEventListener("click", () => {
-
+        lockedRoomBox[i].addEventListener('click', () => {
             for (let y = 0; y < lockedRoomBox.length; y++) {
-                lockedRoomBox[y].style.border = "none"
+                lockedRoomBox[y].style.border = 'none';
             }
             for (let u = 0; u < unlockedRoomBox.length; u++) {
-                unlockedRoomBox[u].style.border = "none"
+                unlockedRoomBox[u].style.border = 'none';
             }
 
             selectedRoom = lockedRoomBox[i].innerText;
-            lockedRoomBox[i].style.border = "2px solid green"
+            lockedRoomBox[i].style.border = '2px solid green';
             console.log(selectedRoom);
-            joinRoom()
-        })
-        
+            joinRoom();
+        });
     }
 
     for (let i = 0; i < unlockedRoomBox.length; i++) {
-        unlockedRoomBox[i].addEventListener("click", () => {
-
+        unlockedRoomBox[i].addEventListener('click', () => {
             for (let y = 0; y < lockedRoomBox.length; y++) {
-                lockedRoomBox[y].style.border = "none"
+                lockedRoomBox[y].style.border = 'none';
             }
             for (let u = 0; u < unlockedRoomBox.length; u++) {
-                unlockedRoomBox[u].style.border = "none"
+                unlockedRoomBox[u].style.border = 'none';
             }
 
             selectedRoom = unlockedRoomBox[i].innerText;
-            unlockedRoomBox[i].style.border = "2px solid green"
+            unlockedRoomBox[i].style.border = '2px solid green';
             console.log(selectedRoom);
-            joinRoom()
-        })
-        
+            joinRoom();
+        });
     }
 }
 
 //What happens when a user joins a room
 function joinRoom() {
-
-    let lockedRoomBox = document.getElementsByClassName("lockedRooms");
-    let unlockedRoomBox = document.getElementsByClassName("unlockedRooms");
+    let lockedRoomBox = document.getElementsByClassName('lockedRooms');
+    let unlockedRoomBox = document.getElementsByClassName('unlockedRooms');
     let locked = false;
 
     for (let i = 0; i < lockedRoomBox.length; i++) {
@@ -149,48 +150,41 @@ function joinRoom() {
         }
     }
 
-
-        let userName = "Testuser"
-        let room = new Room(selectedRoom, locked);
-        socket.emit("join room", {userName, room})
-
-
-
-
-
+    let userName = 'Testuser';
+    let room = new Room(selectedRoom, locked);
+    socket.emit('join room', { userName, room });
 }
 
 //What happens when a user sends a message
 function sendMessage(event) {
-    event.preventDefault()
-    const theInput = document.getElementById("messageInput");
-    let theMessage = theInput.value
+    event.preventDefault();
+    const theInput = document.getElementById('messageInput');
+    let theMessage = theInput.value;
 
-    if (theMessage === "" || theMessage === null) {
+    if (theMessage === '' || theMessage === null) {
         return;
     } else {
-        socket.emit("message", theMessage)
-        theMessage = "";
+        socket.emit('message', theMessage);
+        theMessage = '';
     }
-
-}
-function render (){
-
-
-let frontPage = document.getElementById("frontPage")
-let chatPage = document.getElementById("chatPage")
-let create = document.getElementById("create-rooom")
-
-if (frontPage.style.display != "none") {
-    frontPage.style.display = "none"
-    chatPage.style.display = "block"
-    create.style.display="block"
-} 
-else {
-    frontPage.style.display = "block"
-    create.style.display="block"
-    chatPage.style.display = "none"
-} 
-
 }
 
+let frontPage = document.getElementById('frontPage');
+let chatPage = document.getElementById('chatPage');
+let create = document.getElementById('create-rooom');
+
+function renderHome() {
+    frontPage.style.display = 'block';
+    chatPage.style.display = 'none';
+    create.style.display = 'none';
+}
+function renderChatpage() {
+    chatPage.style.display = 'block';
+    create.style.display = 'none';
+    frontPage.style.display = 'none';
+}
+function renderCreatRooms() {
+    chatPage.style.display = 'none';
+    create.style.display = 'block';
+    frontPage.style.display = 'block';
+}
